64e827a32b4b5815385f76fdc8e56c9290942958
Florentín Pérez González <43187560+FlorentinPG@users.noreply.github.com>
2023-06-09 14:35:50 +0100

    feat(TAP-1300): API Request (#37)

--- client/src/rest/responses.rs
+++ client/src/rest/responses.rs
@@ -1,14 +1,14 @@
+use crate::rest::bodys::PostEventRequestBody;
+use crate::rest::bodys::SignatureRequest;
 use serde::{Deserialize, Serialize};
 use taple_core::identifier::Derivable;
+use taple_core::request::{RequestState, TapleRequest};
 use taple_core::signature::{Signature, SignatureContent};
 use taple_core::{
     Acceptance, Approval, ApprovalContent, ApprovalPetitionData, Evaluation, Event, EventContent,
-    EventProposal, Proposal, SubjectData, SignatureIdentifier,
+    EventProposal, Proposal, SignatureIdentifier, SubjectData,
 };
 use utoipa::ToSchema;
-
-use crate::rest::bodys::PostEventRequestBody;
-use crate::rest::bodys::SignatureRequest;
 
 #[derive(Debug, Clone, Serialize, Deserialize, ToSchema)]
 pub enum AcceptanceResponse {
@@ -209,14 +209,52 @@ impl From<ApprovalPetitionData> for ApprovalPetitionDataResponse {
 #[derive(Debug, Clone, Serialize, Deserialize, ToSchema)]
 pub struct SignatureDataResponse {
     pub content: SignatureContent,
-    pub signature: SignatureIdentifier
+    pub signature: SignatureIdentifier,
 }
 
 impl From<Signature> for SignatureDataResponse {
     fn from(value: Signature) -> Self {
         Self {
             content: value.content,
-            signature: value.signature
+            signature: value.signature,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, ToSchema)]
+pub struct TapleRequestResponse {
+    id: String,
+    subject_id: Option<String>,
+    sn: Option<u64>,
+    event_request: PostEventRequestBody,
+    state: RequestStateResponse,
+}
+
+impl From<TapleRequest> for TapleRequestResponse {
+    fn from(value: TapleRequest) -> Self {
+        Self {
+            id: value.id.to_str(),
+            subject_id: value.subject_id.map(|id| id.to_str()),
+            sn: value.sn,
+            event_request: value.event_request.try_into().unwrap(),
+            state: value.state.into(),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, ToSchema)]
+pub enum RequestStateResponse {
+    Finished,
+    Error,
+    Processing,
+}
+
+impl From<RequestState> for RequestStateResponse {
+    fn from(value: RequestState) -> Self {
+        match value {
+            RequestState::Finished => Self::Finished,
+            RequestState::Error => Self::Error,
+            RequestState::Processing => Self::Processing,
         }
     }
 }
