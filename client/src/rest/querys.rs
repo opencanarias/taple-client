15a6b26296d5a6c02ada4ff8a27976c9066add62
Adrián Padilla Rojas <47563279+padinsky@users.noreply.github.com>
2023-06-13 13:59:33 +0100

    feat(TAP-1310): Pagination in approvals (#40)

--- client/src/rest/querys.rs
+++ client/src/rest/querys.rs
@@ -13,3 +13,9 @@ pub struct GetEventsOfSubjectQuery {
     pub from: Option<i64>,
     pub quantity: Option<i64>,
 }
+
+#[derive(Debug, Clone, Deserialize, IntoParams)]
+#[into_params(parameter_in = Query)]
+pub struct GetApprovalsQuery {
+    pub status: Option<String>
+}
