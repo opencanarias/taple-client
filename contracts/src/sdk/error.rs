2d19c7acf2a37146aecfc3117549b3d4a1456b48
Florentín Pérez González <43187560+FlorentinPG@users.noreply.github.com>
2023-07-10 09:51:07 +0100

    build(TAP-1528): Dockerfile updated for client & tools (#64)

--- contracts/src/sdk/error.rs
+++
@@ -1,9 +0,0 @@
-use thiserror::Error;
-
-#[derive(Error, Debug)]
-pub enum Error {
-  #[error("Serialization Error")]
-  SerializationError,
-  #[error("Deserialization Error")]
-  DeserializationError
-}
