6054fbfe79dbbb3a5255cda61781f262af303712
Eduardo Suárez Ojeda <50803521+edusu@users.noreply.github.com>
2023-08-03 16:05:46 +0200

    fix: removing unnecessary things from taple-sign (#80)
    Co-authored-by: Eduardo Suárez Ojeda <esuarez@opencanarias.es>

--- tools/sign/src/main.rs
+++ tools/sign/src/main.rs
@@ -1,42 +1,35 @@
 use clap::Parser;
+
 use taple_core::signature::Signed;
+
 use taple_core::{signature::Signature, EventRequest};
 
-mod model;
-use model::*;
 #[derive(Parser, Default, Debug)]
 #[clap(
     version,
     about = "TAPLE requests generator utility for invokation to TAPLE nodes"
 )]
+
 struct Args {
     /// Private key to use. HEX String format
     private_key: String,
+
     /// JSON String of the request data to sign
     request: String,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
-    let request = {
-        if args.request.starts_with('\'') || args.request.ends_with('\'') {
-            remove_first_and_last_characters(&args.request)
-        } else {
-            args.request
-        }
-    };
-    let request_body: EventRequestTypeBody = serde_json::from_str(&request)?;
-    let request: EventRequest = request_body.clone().into();
+
+    let request: EventRequest = serde_json::from_str(&args.request)?;
 
     let signature: Signature = Signature::new_from_pk_ed25519(&request, args.private_key)?;
 
-    let signed_event_request = Signed::<EventRequest>::new(request, signature);
+    let signed_request = Signed::<EventRequest>::new(request, signature);
 
-    let result: String = serde_json::to_string_pretty(&signed_event_request)?;
+    let result: String = serde_json::to_string_pretty(&signed_request)?;
+
     println!("{}", result);
-    Ok(())
-}
 
-fn remove_first_and_last_characters(s: &str) -> String {
-    s.chars().skip(1).take(s.len() - 2).collect()
+    Ok(())
 }
