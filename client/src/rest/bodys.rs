3ee9fa109f753a04b161c02118eeb665d40cb6b2
Eduardo Suárez Ojeda <50803521+edusu@users.noreply.github.com>
2023-08-17 13:33:45 +0100

    fix: añadiendo flatten en event request del client para que encaje (#87)
    Co-authored-by: Eduardo Suárez Ojeda <esuarez@opencanarias.es>

--- client/src/rest/bodys.rs
+++ client/src/rest/bodys.rs
@@ -235,6 +235,7 @@ pub struct AuthorizeSubjectBody {
 
 #[derive(Debug, Clone, Serialize, Deserialize, ToSchema)]
 pub struct PostEventRequestBodyPreSignature {
+    #[serde(flatten)]
     /// Type of event request
     pub request: EventRequestBody,
     /// Signature of the issuer
