b342adf0b430146195d591e9ae6cd75bf6895240
Florentín Pérez González <43187560+FlorentinPG@users.noreply.github.com>
2023-09-04 13:57:27 +0100

    fix: Adapted client to new shutdown method at node level (#88)

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,16 +1,17 @@
 extern crate env_logger;
 mod database;
 mod rest;
-use database::leveldb::{open_db, LDBCollection, LevelDBManager};
+use database::leveldb::{open_db, LevelDBManager};
 use log::info;
 use rest::openapi::{serve_swagger, ApiDoc};
 use std::sync::Arc;
 use std::{error::Error, net::SocketAddr};
 use taple_client::{client_settings_builder, ClientSettings, SettingsGenerator};
 use taple_core::crypto::{Ed25519KeyPair, KeyGenerator, KeyPair, Secp256k1KeyPair};
-use taple_core::{KeyDerivator, Taple, TapleShutdownManager};
+use taple_core::{KeyDerivator, Taple, TapleShutdownManager, NodeAPI};
 use tempfile::tempdir as tempdirf;
 use tokio::signal::unix::{signal, SignalKind};
+use tokio::sync::RwLock;
 use utoipa::OpenApi;
 use warp::Filter;
 
@@ -53,30 +54,38 @@ async fn run() -> Result<(), Box<dyn Error>> {
         panic!("No MC available");
     };
     ////////////////////
-    let mut taple = Taple::new(settings.taple.clone(), leveldb);
+    let taple = Taple::new(settings.taple.clone(), leveldb);
+    let api = taple.get_api();
     let shutdown_manager = taple.get_shutdown_manager();
     let signal_shutdown_manager = taple.get_shutdown_manager();
+    let arc_taple = Arc::new(RwLock::new(Some(taple)));
+    let arc_clone = arc_taple.clone();
     let mut stream = signal(SignalKind::terminate())?;
     tokio::task::spawn(async move {
-        let mut inner_receiver = signal_shutdown_manager.get_raw_receiver();
         tokio::select! {
-            _ = inner_receiver.recv() => {
+            _ = signal_shutdown_manager.wait_for_shutdown() => {
 
             },
             _ = stream.recv() => {
-                signal_shutdown_manager.shutdown().await;
+                let mut write_lock = arc_clone.write().await;
+                let taple = write_lock.take();
+                taple.unwrap().shutdown().await;
             }
         };
     });
-    taple.start().await?;
-    info!("Controller ID: {}", taple.controller_id().unwrap());
+    {
+        let mut write_lock = arc_taple.write().await;
+        let taple = write_lock.as_mut().unwrap();
+        taple.start().await?;
+        info!("Controller ID: {}", taple.controller_id().unwrap());
+    }
     if settings.http {
         log::info!(
             "HTTP server listen on {}:{}",
             settings.http_addr,
             settings.http_port
         );
-        start_http_server(settings, taple, keys, derivator, shutdown_manager).await?;
+        start_http_server(settings, api, keys, derivator, shutdown_manager).await?;
     } else {
         log::warn!("HTTP server not enabled");
         shutdown_manager.wait_for_shutdown().await;
@@ -86,7 +95,7 @@ async fn run() -> Result<(), Box<dyn Error>> {
 
 async fn start_http_server(
     settings: ClientSettings,
-    taple: Taple<LevelDBManager, LDBCollection>,
+    taple: NodeAPI,
     keys: KeyPair,
     derivator: KeyDerivator,
     shutdown_manager: TapleShutdownManager,
@@ -112,7 +121,7 @@ async fn start_http_server(
             .and(warp::any().map(move || config.clone()))
             .and_then(serve_swagger);
         warp::serve(api_doc.or(swagger_ui).or(rest::routes::routes(
-            taple.get_api(),
+            taple,
             keys,
             derivator,
         )))
@@ -122,7 +131,7 @@ async fn start_http_server(
         .1
         .await;
     } else {
-        warp::serve(api_doc.or(rest::routes::routes(taple.get_api(), keys, derivator)))
+        warp::serve(api_doc.or(rest::routes::routes(taple, keys, derivator)))
             .bind_with_graceful_shutdown(http_addr, async move {
                 shutdown_manager.wait_for_shutdown().await
             })
